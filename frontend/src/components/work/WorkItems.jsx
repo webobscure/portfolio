4882d72519c549859b57e89b5c667d1be0f522d5
webobscure <sparkygino@gmail.com>
2023-02-13 20:48:29 +0300

    styling examples

--- frontend/src/components/work/WorkItems.jsx
+++ frontend/src/components/work/WorkItems.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import arrow from '../../assets/arrow-right-solid.svg'
 
 const WorkItems = ({ item }) => {
   return (
     <div className="work__card" key={item}>
-        <img src={item.image} alt="example" className='work__img' height={500} weight={500}/>
-        <h3 className="work__title">{item.title}</h3>
-        <a href="#" className="work__button">
-            Demo <i class="fa-solid fa-arrow-right"></i>
-        </a>
+      <img src={item.image} alt="example" className="work__img"   />
+      <h3 className="work__title">{item.title}</h3>
+      <a href="#" className="work__button">
+        Demo
+        <img src={arrow} alt="arrow" className='arrow'/>
+      </a>
     </div>
-    )
-}
+  );
+};
 
-export default WorkItems
+export default WorkItems;
